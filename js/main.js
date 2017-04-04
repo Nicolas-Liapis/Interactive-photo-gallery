0cd8df49ef5e3331343d4b48be0c941745a648a1
Nicolas-Liapis <nicolas.liapis.dev@gmail.com>
2017-04-04 12:31:49 +0100

    Interactive photo gallery v 1.1
    Passed HTML, CSS checks issue with JHint

--- js/main.js
+++ js/main.js
@@ -5,37 +5,17 @@ lightbox.option({
   'wrapAround': true
 });
 
-// var searchImg = document.getElementById('searchB');
-// var liList =  document.getElementsByTagName('li');
-//
-// for ( var i = 0; i < liList.length; i += 1) {
-//   liList[i].style.display ='none';
-// }
-
-// function filterImg();
-//
-// const input = document.querySelector('input');
-//
-// const searchValue = input.value;
-//
-// const li = document.querySelectorAll('li');
-//
-// const title = document.querySelectorAll('li a data-title');
-//
-// if (search.textcontent == li.textcontent);
-
 function filterImg() {
 
     var input, filter, ul, li, a, i;
     // INPUT FUNCTION TO GET SEARCH ITEM
     input = document.getElementById('searchB');
     filter = input.value.toUpperCase();
-
+    //-------------------------------------
     ul = document.getElementById("ulList");
     li = ul.getElementsByTagName('li');
-    // a = ul.getElementsByTagName('a').textcontent();
 
-    // Loop through all list items, and hide those who don't match the search query
+    // LOOPS THROUGH ALL LIST ITEMS TO FIND THE ONE THAT MATCHES
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
